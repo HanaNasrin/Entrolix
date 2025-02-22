6c640ab2477ef80f2f48a0afca6e8504912c6845
shaheema pullisseri <pullisserishaheema@gmail.com>
2025-02-22 16:03:15 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Navbar/>
-=======
+
       <Navbar>
     
->>>>>>> 3098f06511344db2a817efd1eabe2bc519c9c1de
+
         <BrowserRouter>
           <Routes>
               <Route path='/' element={<Home/>} />
           </Routes>  
         </BrowserRouter>
-<<<<<<< HEAD
+
       <Footer/>
      
    
-=======
+
       </Navbar>
       <Home/>
->>>>>>> 3098f06511344db2a817efd1eabe2bc519c9c1de
+
     </div>
   );
 }
