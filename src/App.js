3098f06511344db2a817efd1eabe2bc519c9c1de
Rizvaaa <pprisva22@gmail.com>
2025-02-22 15:42:56 -0800

    chnging

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Home from './Components/Home';
+import Home from './pages/Home';
 import Navbar from './Components/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 
 function App() {
   return (
     <div className="App">
       <Navbar>
+    
         <BrowserRouter>
           <Routes>
               <Route path='/' element={<Home/>} />
           </Routes>  
         </BrowserRouter>
       </Navbar>
+      <Home/>
     </div>
   );
 }
