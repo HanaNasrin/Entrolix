669935a16d80e41a33f79a1b92c7492d98fc4d00
shaheema pullisseri <pullisserishaheema@gmail.com>
2025-02-27 11:50:33 +0530

    app

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<SignUp/>}/>
               <Route path='/adminlogin' element={<AdminLogin/>}/>
+              
           </Routes>  
         </BrowserRouter>
       <Footer/>
